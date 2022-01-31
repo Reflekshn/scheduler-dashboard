3e6b13dede06fcf0c3e9007d6cfaa69f517b3e56
Reflekshn <reflekshn@hotmail.com>
2022-01-31 18:33:28 -0500

    Implement initial Loading Component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import classNames from 'classnames';
+
+class Loading extends Component {
+  render() {
+    return <section className="loading">Loading</section>;
+  }
+}
+
+export default Loading;
