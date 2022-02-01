b61e9fbdb63a2505202a52f556ee85d051ccc869
Reflekshn <reflekshn@hotmail.com>
2022-02-01 14:37:22 -0500

    Implement web socket connection to listen for changes when booking interviews with scheduler app

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,7 +11,9 @@ import {
   getMostPopularDay,
   getInterviewsPerDay,
 } from 'helpers/selectors';
+import { setInterview } from 'helpers/reducers';
 
+// Panel data array
 const data = [
   {
     id: 1,
@@ -64,20 +66,40 @@ class Dashboard extends Component {
     // Initiate a WebSocket connection
     this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
 
-    //
+    // Listen for messages on the socket connection and use them to update the
+    // state when we book or cancel an interview
+    this.socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      if (typeof data === 'object' && data.type === 'SET_INTERVIEW') {
+        this.setState((previousState) =>
+          setInterview(previousState.data.id, data.interview)
+        );
+      }
+    };
+
+    // Store the focused state using Local Storage
     const focused = JSON.parse(localStorage.getItem('focused'));
 
+    // Set the new value of focused if one exits (a panel has been clicked on)
     if (focused) {
       this.setState({ focused });
     }
   }
 
+  // Update the previous state when the component updates
   componentDidUpdate(prevProps, prevState) {
     if (prevState.focused !== this.state.focused) {
       localStorage.setItem('focused', JSON.stringify(this.state.focused));
     }
   }
 
+  // Close the WebSocket connection
+  componentWillUnmount() {
+    this.socket.close();
+  }
+
+  // Select the panel by setting the focused variable to the panel id
   selectPanel(id) {
     this.setState((prev) => ({
       focused: prev.focused !== null ? null : id,
@@ -85,14 +107,19 @@ class Dashboard extends Component {
   }
 
   render() {
+    // Create a variable to store the css classes
     const dashboardClasses = classnames('dashboard', {
+      // Set focused class if the focused state is not null
       'dashboard--focused': this.state.focused,
     });
 
+    // Display the Loading component if the loading state is true
     if (this.state.loading) {
       return <Loading />;
     }
 
+    // Filter the current panel if selected and pass it to the Panel Component,
+    // otherwise display all the panels using the map function
     const panels = data
       .filter(
         (panel) =>
@@ -108,6 +135,7 @@ class Dashboard extends Component {
         />
       ));
 
+    // Return the panels object to display
     return <main className={dashboardClasses}>{panels}</main>;
   }
 }
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 
 class Panel extends Component {
   render() {
+    // Destructure the props we want to use
     const { label, value, onSelect } = this.props;
 
+    // Display the current panel using the prop values, and set the onClick function
     return (
       <section className="dashboard__panel" onClick={onSelect}>
         <h1 className="dashboard__panel-header">{label}</h1>
